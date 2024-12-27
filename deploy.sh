5f800555ed113a995c89a7d9c2769995f7babd38
rick tait <rick.tait@hypermediasystems.com>
2024-12-27 10:22:20 -0800

    added support for GCP_SERVICE_ACCOUNT and --service-account= on deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@
 echo -e "${YELLOW}***"
 echo -e "*** running: ${GREEN} ---> ${RED} gcloud config set project ${PROJECT_ID}${RESET}"
 gcloud config set project ${PROJECT_ID}
-echo -e "*** running: ${GREEN} ---> ${RED} gcloud beta run deploy ${ENDPOINT} --region ${GCP_REGION} --image gcr.io/${PROJECT_ID}/${ENDPOINT} --port ${RUNPORT} --cpu 1 --memory 1Gi --max-instances ${MAX_INSTANCES} --min-instances ${MIN_INSTANCES} --no-cpu-boost --cpu-throttling --allow-unauthenticated ${RESET}"
+echo -e "*** running: ${GREEN} ---> ${RED} gcloud beta run deploy ${ENDPOINT} --region ${GCP_REGION} --image gcr.io/${PROJECT_ID}/${ENDPOINT} --port ${RUNPORT} --cpu 1 --memory 1Gi --max-instances ${MAX_INSTANCES} --min-instances ${MIN_INSTANCES} --no-cpu-boost --cpu-throttling --allow-unauthenticated ${RESET} --service-account=${GCP_SERVICE_ACCOUNT}"
 echo -e "${YELLOW}***"
 
 gcloud beta run deploy ${ENDPOINT} \
@@ -22,6 +22,7 @@ gcloud beta run deploy ${ENDPOINT} \
     --min-instances ${MIN_INSTANCES} \
     --no-cpu-boost \
     --cpu-throttling \
-    --allow-unauthenticated
+    --allow-unauthenticated \
+	 --service-account=${GCP_SERVICE_ACCOUNT}
 
 # EOF
